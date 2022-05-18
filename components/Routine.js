6088bb7ecc24d27932ddb43d31474c6b158b75e3
Tim Benniks <tbenniks@gmail.com>
2022-05-18 17:58:30 +0200

    routine added

---
+++ components/Routine.js
@@ -0,0 +1,36 @@
+export default function Hero({ component }) {
+  const { srcset } = component.parameters.cloudinary.value[0];
+  const url = component.parameters.url.value;
+  const { chapeau, copy, cta, title } = component.parameters.entry.value;
+
+  return (
+    <section className="py-16 relative bg-light">
+      <img
+        className="w-2/4"
+        width={805}
+        height={535}
+        srcSet={srcset}
+        alt={title}
+      />
+
+      <article className="bg-tertiary p-8 max-w-2xl absolute top-32 right-32">
+        <h2 className="mb-8">
+          <span className="block text-primary font-semibold text-6xl">
+            {chapeau}
+          </span>
+          <span className="block text-dark font-semibold text-6xl ml-8 -mt-2">
+            {title}
+          </span>
+        </h2>
+
+        <p className="text-xl ml-8 mb-8">{copy}</p>
+        <a
+          href="#"
+          className="inline-block uppercase font-semibold px-4 py-3 bg-primary text-light ml-8"
+        >
+          {cta}
+        </a>
+      </article>
+    </section>
+  );
+}
