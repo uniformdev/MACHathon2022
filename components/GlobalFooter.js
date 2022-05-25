ca188416109347df98e92e502d2b922b585340b4
Ante Sepic <antesepic@protonmail.com>
2022-05-25 15:26:43 +0200

    :wrench: Partially fix preview mode

--- components/GlobalFooter.js
+++ components/GlobalFooter.js
@@ -1,25 +1,27 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const GlobalHeader = () => {
+  const router = useRouter();
+
   return (
     <header className="bg-dark h-72 mx-auto" style={{ maxWidth: "1440px" }}>
       <div className="mx-auto max-w-screen-xl flex flex-col justify-between text-center pt-24">
-        <Link href="/">
+        <Link href="/" prefetch={!router.isPreview}>
           <a className="text-light font-semibold text-3xl">skncre</a>
         </Link>
-
         <nav className="mt-6 mr-5 text-xl space-x-6 hidden sm:block">
-          <Link href="/">
+          <Link href="/pdp/face-serum" prefetch={!router.isPreview}>
             <a className="text-light">serum</a>
           </Link>
-          <Link href="/">
+          <Link href="/pdp/face-cream" prefetch={!router.isPreview}>
             <a className="text-light">cream</a>
           </Link>
-          <Link href="/">
+          <Link href="/pdp/eye-contour" prefetch={!router.isPreview}>
             <a className="text-light">eye contour</a>
           </Link>
-          <Link href="/">
-            <a className="text-light">about us</a>
+          <Link href="/pdp/bundle" prefetch={!router.isPreview}>
+            <a className="text-light">skncre bundle</a>
           </Link>
         </nav>
       </div>
--- components/GlobalHeader.js
+++ components/GlobalHeader.js
@@ -1,27 +1,30 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const GlobalHeader = () => {
+  const router = useRouter();
+
   return (
     <header className="bg-dark h-20 mx-auto" style={{ maxWidth: "1440px" }}>
       <div
         className="mx-auto h-20 flex justify-between"
         style={{ maxWidth: "1440px" }}
       >
-        <Link href="/">
+        <Link href="/" prefetch={!router.isPreview}>
           <a className="text-light font-semibold text-3xl mt-5 ml-8">skncre</a>
         </Link>
 
         <nav className="mt-6 mr-8 text-xl space-x-6 hidden sm:block">
-          <Link href="/pdp/face-serum">
+          <Link href="/pdp/face-serum" prefetch={!router.isPreview}>
             <a className="text-light">face serum</a>
           </Link>
-          <Link href="/pdp/face-cream">
+          <Link href="/pdp/face-cream" prefetch={!router.isPreview}>
             <a className="text-light">face cream</a>
           </Link>
-          <Link href="/pdp/eye-contour">
+          <Link href="/pdp/eye-contour" prefetch={!router.isPreview}>
             <a className="text-light">eye contour</a>
           </Link>
-          <Link href="/pdp/bundle">
+          <Link href="/pdp/bundle" prefetch={!router.isPreview}>
             <a className="text-light">skncre bundle</a>
           </Link>
         </nav>
