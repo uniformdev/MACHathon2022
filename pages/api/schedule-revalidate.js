5fc278bee6714747e2c13e251e8f7519309ecd96
Ante Sepic <antesepic@protonmail.com>
2022-05-26 11:40:18 +0200

    :wrench: Add a secondary revalidation hook that responds immediately
    ... to avoid timeouts

---
+++ pages/api/schedule-revalidate.js
@@ -0,0 +1,17 @@
+// This hook only calls the actual revalidate hook and immediately returns
+// This is to avoid running into timeout problems
+
+export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.NEXT_REVALIDATE_SECRET) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  fetch(
+    `${req.headers.host.includes("localhost") ? "http" : "https"}://${
+      req.headers.host
+    }/api/revalidate?secret=${process.env.NEXT_REVALIDATE_SECRET}`
+  );
+
+  return res.status(200).send();
+}
