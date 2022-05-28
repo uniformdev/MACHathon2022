78097071921f0a61ad6bd55fe2950d2a50a51991
Ante Sepic <antesepic@protonmail.com>
2022-05-28 18:37:59 +0200

    :sparkles: Add personalization

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import "../styles/globals.css";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { UniformContext } from "@uniformdev/context-react";
+import { Context, enableContextDevTools } from "@uniformdev/context";
+import manifest from "../lib/contextManifest.json";
+
+const context = new Context({
+  manifest,
+  defaultConsent: true,
+  plugins: [enableContextDevTools()],
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <ParallaxProvider>
-      <Component {...pageProps} />
+      <UniformContext context={context}>
+        <Component {...pageProps} />
+      </UniformContext>
     </ParallaxProvider>
   );
 }
