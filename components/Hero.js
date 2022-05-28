b6bd370bea4afc1a679996ad936632b727f57d9e
Ante Sepic <antesepic@protonmail.com>
2022-05-28 18:37:44 +0200

    :bug: Fix Hero client-side personalization compatibility

--- components/Hero.js
+++ components/Hero.js
@@ -1,27 +1,33 @@
 import { Parallax } from "react-scroll-parallax";
 
 export default function Hero({ component }) {
-  const { srcset } = component.parameters.cloudinary.value[0];
+  const { srcset } = component.parameters.cloudinary?.value[0];
   const { title, subtitle } = component.parameters.entry.value;
-
-  function addColorToYour(title) {
-    return title.replace("your", '<span class="text-primary">your</span>');
-  }
+  const titleParts = title.split("your");
 
   return (
     <section className="aspect-[1440/632] relative">
-      <img
-        width={1440}
-        height={632}
-        srcSet={srcset}
-        alt="rediscover your skin"
-        className="absolute"
-      />
-      <Parallax className="absolute max-w-xl top-56 left-56" speed={10}>
-        <h1
-          className="font-semibold text-8xl mb-8"
-          dangerouslySetInnerHTML={{ __html: addColorToYour(title) }}
+      {srcset ? (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          width={1440}
+          height={632}
+          srcSet={srcset}
+          alt="rediscover your skin"
+          className="absolute"
         />
+      ) : null}
+      <Parallax className="absolute max-w-xl top-44 left-56" speed={10}>
+        <h1 className="font-semibold text-6xl mb-8 xl:text-7xl 2xl:text-8xl">
+          {titleParts.length > 1 ? (
+            <>
+              {titleParts[0]} <span className="text-primary">your</span>{" "}
+              {titleParts[1]}
+            </>
+          ) : (
+            title
+          )}
+        </h1>
         <p className="text-xl">{subtitle}</p>
       </Parallax>
     </section>
